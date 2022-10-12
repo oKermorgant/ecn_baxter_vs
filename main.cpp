7e8560aa4c9e9e97115f0ef716c1bcb0256275b2
Olivier Kermorgant <olivier_kermorgant@yahoo.fr>
2022-10-12 16:05:41 +0200

    ros_baxter

--- main.cpp
+++ main.cpp
@@ -8,52 +8,51 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
-    BaxterArm arm(argc, argv, "right");    // defaults to right arm
+  BaxterArm arm(argc, argv, "right");    // defaults to right arm
 
-    vpColVector q = arm.init();
+  vpColVector q = arm.init();
 
-    vpColVector qmin = arm.jointMin(),
-                qmax = arm.jointMax();
+  vpColVector qmin = arm.jointMin(), qmax = arm.jointMax();
 
-    // define a simple 2D point feature and its desired value
-    vpFeaturePoint p,pd;
-    pd.set_xyZ(0,0,1);
+  // define a simple 2D point feature and its desired value
+  vpFeaturePoint p,pd;
+  pd.set_xyZ(0,0,1);
 
-    // the error
-    vpColVector e(3);
-    double x, y, area;
-    // desired area
-    const double area_d = arm.area_d();
+  // the error
+  vpColVector e(3);
+  double x, y, area;
+  // desired area
+  const double area_d = arm.area_d();
 
-    // loop variables
-    vpColVector qdot(7);
-    vpMatrix L(3, 6), Js(3,7);
+  // loop variables
+  vpColVector qdot(7);
+  vpMatrix L(3, 6), Js(3,7);
 
-    while(arm.ok())
-        {
-            cout << "-------------" << endl;
+  while(arm.ok())
+  {
+    cout << "-------------" << endl;
 
-            // get point features
-            x = arm.x();
-            y = arm.y();
-            area = arm.area();
-            p.set_xyZ(x,y, 1);
-            std::cout << "x: " << x << ", y: " << y << ", area: " << area << '\n';
+    // get point features
+    x = arm.x();
+    y = arm.y();
+    area = arm.area();
+    p.set_xyZ(x,y, 1);
+    std::cout << "x: " << x << ", y: " << y << ", area: " << area << '\n';
 
-            // update error vector e
+    // update error vector e
 
-            // update interaction matrix L
+    // update interaction matrix L
 
-            // compute feature Jacobian from L and cameraJacobian
+    // compute feature Jacobian from L and cameraJacobian
+    arm.cameraJacobian();
+    // build H matrix (2nd section) using arm.rho()
+    q = arm.jointPosition();
 
-            // build H matrix (2nd section) using arm.rho()
-            q = arm.jointPosition();
 
+    // send this command to the robot
+    arm.setJointVelocity(qdot);
 
-            // send this command to the robot
-            arm.setJointVelocity(qdot);
-
-            // display current joint positions and VS error
-            arm.plot(e);
-    }
+    // display current joint positions and VS error
+    arm.plot(e);
+  }
 }
